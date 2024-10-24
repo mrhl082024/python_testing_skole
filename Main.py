efc5b13d778bdf9916d9659e4a7455ac4727077a
mrhl082024 <mhrl082024@gmail.com>
2024-10-24 10:17:25 +0200

    first, med noen lister

---
+++ Main.py
@@ -0,0 +1,12 @@
+sorting_list=[2,4,3,6,8,7,9,1,5]
+sorting_list.sort()
+print(sorting_list)
+sorting_list.sort(reverse=True)
+print(sorting_list)
+
+
+adding_list=["volvo", "audi"]
+adding_list.append("bmw")
+print(adding_list)
+adding_list.pop(0)
+print(adding_list)
