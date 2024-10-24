427db07aecb17075dbde47a4755f53c25bd25510
mrhl082024 <mhrl082024@gmail.com>
2024-10-24 13:11:20 +0200

    laget en tuple med flere foskjellige måter å få ut svar på

--- Main.py
+++ Main.py
@@ -10,3 +10,14 @@ adding_list.append("bmw")
 print(adding_list)
 adding_list.pop(0)
 print(adding_list)
+
+
+#tuple
+mytuple=("apple", "banana", "pear", "apple")
+print(len(mytuple))
+print(type(mytuple))
+print(mytuple[1])
+print(mytuple[-1])
+print(mytuple[1:3])
+if "pear" in mytuple:
+    print("there is a pear in this tuple")
