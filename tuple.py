d55059c523404015458f5e4321c28a72b7f6e04e
mrhl082024 <mhrl082024@gmail.com>
2024-10-25 09:08:42 +0200

    flyttet en del kode over til tuple

--- tuple.py
+++ tuple.py
@@ -1,3 +1,39 @@
+
+
+#tuple
+mytuple=("apple", "banana", "pear", "apple")
+print(len(mytuple))
+print(type(mytuple))
+print(mytuple[1])
+print(mytuple[-1])
+print(mytuple[1:3])
+if "pear" in mytuple:
+    print("there is a pear in this tuple")
+
+
+x = ("bmw", "audi", "volvo")
+y = list(x)
+y[1] = "mercedes"
+x = tuple(y)
+print(x)
+
+y = list(x)
+y.append("alfa romeo")
+x = tuple(y)
+print(x)
+
+y = list(x)
+y.remove("alfa romeo")
+x = tuple(y)
+print(x)
+
+tuple_fruits = ("apple", "kiwi", "orange")
+(red, green, yellow) = tuple_fruits
+print(green)
+print(red)
+print(yellow)
+
+
 mytuple = ("Volvo", "BMW", "Audi", "Mercedez")
 for x in mytuple:
     print(x)
@@ -10,3 +46,4 @@ y = 0
 while y < len(fruit_tuple):
     print(fruit_tuple[y])
     y = y + 1
+
