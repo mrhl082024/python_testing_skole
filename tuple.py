67c3703e23c482c713fee0c08036db8d5ebb0866
mrhl082024 <mhrl082024@gmail.com>
2024-10-25 08:55:13 +0200

    lager for loop og while loop

--- tuple.py
+++ tuple.py
@@ -1,3 +1,12 @@
 mytuple = ("Volvo", "BMW", "Audi", "Mercedez")
 for x in mytuple:
     print(x)
+
+for i in range(len(mytuple)):
+    print(mytuple[i])
+
+fruit_tuple = ("apple", "banana", "mango")
+y = 0
+while y < len(fruit_tuple):
+    print(fruit_tuple[y])
+    y = y + 1
