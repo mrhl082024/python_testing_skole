8da536d1fa20a7135b1a9626af0d78763c5be069
mrhl082024 <mrhl082024@gmail.com>
2024-10-28 13:14:29 +0100

    update set

--- set.py
+++ set.py
@@ -10,3 +10,10 @@ set2 = {"enda", "flere", "ord"}
 for x in set2:
     print(x)
 print("enda" in set2)
+
+set3 = {"volvo", "bmw", "audi"}
+set3.add("volkswagon")
+print(set3)
+
+set3.update(set2)
+print(set3)
